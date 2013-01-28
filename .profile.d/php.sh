146ab624e25331196c8857d369c495421086ef38
Alan Pinstein <apinstein@mac.com>
2013-01-28 13:40:44 -0500

    Try profile.d script again.

--- .profile.d/php.sh
+++ .profile.d/php.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-export PATH=/app/bin:/app/apache/bin:/app/php/bin:$PATH
-# it's bad (security-wise) to use LD_LIBRARY_PATH; need to fix our build process so that the apps know to look in /app/... but not sure how yet...
-export LD_LIBRARY_PATH=/app/apache/lib:/app/lib
+#!/bin/sh
+echo "php.sh"
+
+export PATH=/app/vendor/php/bin:/app/vendor/apache/bin:$PATH
 
 alias l='ls -al'
